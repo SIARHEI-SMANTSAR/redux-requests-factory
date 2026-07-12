c70df1fb66206b3c9785f9db2b662edd76b40208
SIARHEI-SMANTSAR <siarhei.smantsar@gmail.com>
2026-07-12 19:19:18 +0200

    migrate to tsup

---
+++ tsup.config.js
@@ -0,0 +1,23 @@
+const common = {
+  entry: ['src/index.ts'],
+  sourcemap: true,
+  target: 'es5',
+  splitting: false,
+};
+
+module.exports = [
+  {
+    ...common,
+    format: ['cjs'],
+    dts: true,
+    clean: true,
+    outExtension: () => ({ js: '.js' }),
+  },
+  {
+    ...common,
+    format: ['esm'],
+    dts: false,
+    clean: false,
+    outExtension: () => ({ js: '.esm.js' }),
+  },
+];
